f0c6e822552ce1e3de253cfe87aafbbf9d76d361
kienty <897432975@qq.com>
2017-12-18 15:30:11 +0800

    使用介绍
    - (void)Choice:(NSInteger)index;//设置默认选中的index
    Copy void(^ChoiceBlock)(NSInteger tag);//点击选择的回调

--- Choice/DetailsHeaderView.h
+++ Choice/DetailsHeaderView.h
@@ -16,6 +16,6 @@ Strong UILabel *xhLabel;
 - (void)createUI:(NSArray *)titleArray
          noteImg:(NSArray *)imgArray
         clickImg:(NSArray *)array;
-- (void)Choice:(NSInteger)index;
-Copy void(^ChoiceBlock)(NSInteger tag);
+- (void)Choice:(NSInteger)index;//设置默认选中的index
+Copy void(^ChoiceBlock)(NSInteger tag);//点击选择的回调
 @end
